9b862e9987f93375bab50528d095ecfc6d830c1e
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 17:26:19 -0600

    Create Grupo-de-los-geniales.py

---
+++ Grupo-de-los-geniales.py
@@ -0,0 +1,30 @@
+opcion=int(input('''Qué quieres hacer
+ 1.Saber si tienes tocayo 
+ 2. Hacer una potencia 
+ 3.Saludar con tu edad'''))
+print(" ")
+if opcion == 1 : 
+ v1=str(input("ingresa tu nombre crack"))
+ print(" ")
+ v2=str(input("ingresa tu nombre crack dois"))
+ print(" ")
+ if v1 == v2 :
+  print("si sos")
+ else:
+  print("no sos")
+
+
+if opcion == 2 :
+ n1=float(input("Ingrese um numero"))
+ n2=float(input("ingrese el siguente numero"))
+res=n1**n2   
+print(res)
+
+if opcion == 3 :
+ e1=str(input("Ingrese su nombre"))
+ e2=int(input("Ingrese su ano do nacimento"))
+ edad= 2022-e2
+ print("Hola,",e1,"este ano cumples: ", edad)
+print(" ")
+print("bueno chavales este fue el video de esta semana espero os haya gustado y nos vemos en el proximo")
+#Este programa tiene multiples tareas que el mismo programa te dicen que son 
