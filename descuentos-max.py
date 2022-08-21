ec72243e7f37769f6febc7d4876b81429c7d4b0d
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 17:41:35 -0600

    Create descuentos-max.py

---
+++ descuentos-max.py
@@ -0,0 +1,13 @@
+print("Bienvenidos a MAX")
+precio= float(input("Ingrese precio"))
+cantidad= float(input("ingrese la cantidad de su pruducto"))
+totaln= float
+totaln= precio*cantidad
+if cantidad>=5:
+  preciodes= totaln*0.4
+  ahorro= -preciodes+totaln
+  print("Su total sin descuento: Q.",totaln)
+  print("Su ahorro es de: Q.",ahorro)
+  print("Su total con descuento: Q.",preciodes) 
+else: print("su total es", totaln)
+#Este programa hace una operacion para sacarle unos descuentos 
