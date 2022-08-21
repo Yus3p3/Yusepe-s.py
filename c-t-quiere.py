b4cb6044508f312ccb587d5f7dfe692960c745f0
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 15:03:01 -0600

    Create c-t-quiere.py

---
+++ c-t-quiere.py
@@ -0,0 +1,24 @@
+n1=25465
+n2=75325
+print("Te quiero mucho persona agradable que este viendo esto <3")
+print(" ")
+print("esto es una suma :)")
+res=n1+n2
+print(res)
+res1=n1-n2
+print(" ")
+print("Esto es una resta")
+print(res1)
+res2=n1*n2
+res3=n1/n2
+res4=n1%n2
+print(" ")
+print("esto es una multiplicacion crack")
+print(res2)
+print(" ")
+print("Esto es una Divicion Eco Eco")
+print(res3)
+print(" ")
+print("Y por ultimo pero no menos importante el modulo papa")
+print(res4)
+#estos fueron mis inicios en phyton este programa hace sumas, restas, divisiones y multiplicaciones sin pedirle ningun numero al usuario
