8164846f573419b9d64dc6f09fc762c497a98c33
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 20:34:45 -0600

    Create radio-cilindro.py

---
+++ radio-cilindro.py
@@ -0,0 +1,13 @@
+print("Hola Kpo, mi nombre es cilindriz, y te ayudare a sacarle el volumen a un cilindro :)")
+print(" ")
+al=float(input("primero dime,  ¿ cual es la altura de tu cilindro?"))
+#Le preguntamos la altura
+print(" ")
+r=float(input("Muy bien ahora, dime cual es el radio de tu cilidro?"))
+#Le preguntamos el radio
+a=3.1415*r**2
+#Sacamos el area
+ed=a*r
+#Volumen
+print("Muy bien, el columen de su cilindro es:", ed)
+#Imprimimos el final 
