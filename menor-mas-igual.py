7fe009f703e6387de0993ae1e56e2739f0c19ec6
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 18:12:57 -0600

    Create menor-mas-igual.py

---
+++ menor-mas-igual.py
@@ -0,0 +1,9 @@
+n1=int(input("Escribe tu primer numero"))
+n2=int(input("Escribe el segundo numero"))
+if n1>n2:
+ print("Este es el numero menor", n2)
+elif n1<n2:
+  print("Este es el numero menor", n1)
+elif n1==n2:
+  print("son iguales")
+  #Este programa te dice si el primer numero es mayor, menor o igual que el segundo caracter que el usuario eligio 
