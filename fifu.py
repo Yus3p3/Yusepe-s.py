c5a6a65aee9a71ed4bdcfef12feb180f38315deb
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 19:51:53 -0600

    Create fifu.py

---
+++ fifu.py
@@ -0,0 +1,15 @@
+print("Hola a todos, bienvenidos a la calculadora de Fisica fundamental basica")
+
+first=int(input("Ingrese que quieres calcular: 1.masa 2.peso 3.fuerza Pd: Recuerden poner solo el numero ")
+
+if first == 1:
+  p=input("Ingrese peso")
+  print(p/9,8)
+if first == 2:
+  m=input("Ingrese masa")
+  print(m* 9.8)
+if first==3:
+ w=input("ingrese masa")
+ a=input("ingrese aceleracion")
+  print(w*a)
+  #Te ayda aresolver problemas basicos de fifu con formulas 
