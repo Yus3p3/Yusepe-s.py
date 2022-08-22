065402dca563812bfb06e8aef1696e9be6123bfd
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 20:25:13 -0600

    Update terrenos-de-la-abuela.py

--- terrenos-de-la-abuela.py
+++ terrenos-de-la-abuela.py
@@ -1,23 +1,25 @@
-pre=1 
+print("Bienvenido al programa para saber el area del terreno que te heredo tu tío el narco :D")
+print(" ")
+p=1 
+
 def a (x,y):
-   r=x*y
-x=float(input("Cual es el ancho del terreno que su abuela le quiere donar?: "))
-y=float(input("Cual es el alto del terreno de su querida abuelia?")
-print("El area de su terreno es de:", r)
-        
+    x=float(input("Cual es el ancho del terreno?: "))
+    y=float(input("Cual es el alto del terreno?: "))
+    r=x*y
+    print("el area del terreno es de ", r)
 
 
   
-while a == 1:
+while p == 1:
    a(1,2)
    
-   a=int(input("Desea continuar? Si. 1, No. 2 "))
-   if a == 2:
+   i=int(input("Quiere calcular otro terreno? Si. 1, No. 2 "))
+   if i == 2:
       break
-   elif a == 1: 
-      a= 1
+   elif i == 1: 
+      pre= 1
    
    else:
-    print("Ingrese una opcion correcta ")
-    a=int(input("Quiere continuar? Si. 1, No. 2 "))
+    print("Solamente puede ingresar 1 o 2 ")
+    pre=int(input("Quiere continuar? Si. 1, No. 2 "))
     #Calcula el area del terreno y ademas pregunta si desea continuar
