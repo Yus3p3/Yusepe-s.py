becebc98b1d36697b1959462be4eea79031c957a
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 18:58:51 -0600

    Create Ala-que-coincidencia.py

---
+++ Ala-que-coincidencia.py
@@ -0,0 +1,11 @@
+n1=str(input("ingrese el primer nombre"))
+print(" ")
+n2=str(input("ingrese segundo nombre"))
+print(" ")
+if n1[0]== n2[0]:
+ print("Alaa mira que coincidencia la primera letra de su nombre son iguales, es la letra", n1[0])
+elif n1[-1]==n2[-1]:
+ print("ALAAAAA mira que coincidencia, la ultima letra de sus nombres son iguales, es la letra ", n1[-1])
+else:
+  print("no hay ninguna coincidencia :( ")
+  #Este programa compara los nombre de los usuarios para ver si la primera o la ultima letra de su nombre coiciden
