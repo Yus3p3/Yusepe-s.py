ff7059fe0309fbb1a1ebedfc7202a6db2b2bc429
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 18:04:58 -0600

    Create quien-es-menor.py

---
+++ quien-es-menor.py
@@ -0,0 +1,7 @@
+n1=int(input("Escribe tu primer numero"))
+n2=int(input("Escribe el segundo numero"))
+if n1>n2:
+ print("Este es el numero menor", n2)
+else:
+  print("Este es el numero menor", n1)
+  #Te menciona que numero es menor
