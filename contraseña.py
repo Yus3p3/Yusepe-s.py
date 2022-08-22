c6d2115945d67a696b7d4241e6a8ae24f77b5cc9
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 19:23:31 -0600

    Create contraseña.py

---
+++ contraseña.py
@@ -0,0 +1,6 @@
+c=input("ingrese contraseña")
+while c != "ankaramessi":
+ print =(input(" error, vuelva a intetarlo"))
+ c=input("Ingrese contraseña")
+else:print("Genio entraste a la pc")
+#Este programa dpende de lo que pongas como contraseña te va a dejar entrar a la base de datos o no 
