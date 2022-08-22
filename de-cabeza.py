f625377bc89880e6e18e6382ab464c83e4d6af3d
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 20:12:18 -0600

    Create de-cabeza.py

---
+++ de-cabeza.py
@@ -0,0 +1,4 @@
+texto= str(input("Ingrese la palabra a la que le quiere aplicar la fonetica inversa"))
+x= texto[::-1]
+print(x)
+#El texto que ingresa el usuario lo pone al reves con la funcion de :-1 que voltea lo que puso el usuario 
