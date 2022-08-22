f287a953d06552bce97beafa6a253e7d6bf4e241
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 19:01:36 -0600

    Create las-elecciones.py

---
+++ las-elecciones.py
@@ -0,0 +1,13 @@
+print("Bienvenido a las elcciones del presidente de la clase :), Las opcinones son a:Rojo b:Verde c: Azul. Recuerde votar por el que más le guste, no se deje comprar por una lamina")
+print(" ")
+c=str(input("El candidato que quiero que me gobierne es: a, b, c?"))
+print(" ")
+if c == ("a"):
+  print("Usted a elegido al candidato rojo, es algo corrupto el tipo pero se le quiere de todas formas")
+elif c == ("b"):
+  print("Usted a elegido al candidato verde, Wuuuuuuuuuuuuuu verde, el nos va a salvar de la pobreza escolar!!! PD: Este programa no tiene preferencias, solo dice la verdad")
+elif c == ("c"):
+  print("A elegido al candidato azul, me huele a que el si te compro con una lamina, pero confiamos")
+else:
+  print(" opcion errona. recuerde que solo puede elegir entre a b c. intente de nuevo, dele a Run nuevamente")
+  #Este programa sirve para hacer votaciones y para que los alumnos hagan sus elecciones 
