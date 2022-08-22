86e3c6cbbf6388a7d09b131744ea8f157cdcad54
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 18:53:23 -0600

    Create la-semana.py

---
+++ la-semana.py
@@ -0,0 +1,12 @@
+dia=str(input("Ingrese el dia de la semana en la que estas"))
+print(" ")
+if dia == "lunes":
+  print("Estas en el inicio de semana ¡A por todo crack! da lo mejor de vos")
+elif dia == "viernes":
+  print("WUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU ES VIERNES VAMOOOOOOOOOOOOOOOOOOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+elif dia == "sabado":
+  print("Que lindo que es el sabado ¡Miramos una pelicula?")
+elif dia == "domingo":
+  print("no te olvides de disfrutar este ultimo dia de fin de semana")
+else:print("estas en un dia x, no tengo ganas de darte un mensaje motivacional")
+#este programa te da mensajes motivacionales dependiendo del dia en el que te encuentras
