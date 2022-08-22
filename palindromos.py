00d2edd2c64fd793d9a3da96a5dfb2e3f73a97f5
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 20:23:23 -0600

    Create palindromos.py

---
+++ palindromos.py
@@ -0,0 +1,6 @@
+p=str(input("Ingrese una palabra para ver si es un palindromo"))
+if str(p)[::-1] == str(p) :
+    print("Felicidades, su palabre se lee igual al derecho y al reves, osea que es un palindromo :D ")
+else:
+    print("No hay palindromo D:")
+    #Este programa mira si se lee lo mismo al derecho y al reves
