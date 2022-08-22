61ad10a335de1133f3acbaadb46e639f971a9d67
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 18:55:43 -0600

    Create valor-absoluto.py

---
+++ valor-absoluto.py
@@ -0,0 +1,7 @@
+n=int(input("Ingrese un numero para ver su valor absoluto"))
+print(" ")
+if n>0:
+  print("este es su valor total", n)
+elif n<0:
+ print("este es su valor total", (n*-1))
+ #Este program te dice el valor absoluto del numero, si el numero es menor a 0 lo multiplica por -1 para volverlo positivo 
