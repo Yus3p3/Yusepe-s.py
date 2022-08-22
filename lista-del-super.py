c22fb2078f514fefa8be731f042488b7c07d2606
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 20:07:49 -0600

    Create lista-del-super.py

---
+++ lista-del-super.py
@@ -0,0 +1,9 @@
+lista= []
+c=str
+while c != "fin" :
+ c=input("Ingrese que quiere meter a su lista para el walmart. Si desea dejar de escrbir y ver la lista ponga (fin)")
+ lista.append(c)
+
+else :
+  print(" " and "La lista quedaria:", lista)
+  #Este programa crea una lista para que almacenes cosas que deseas comprar, el while sirve para seguir almacenando cosas a la lista hasta que se coloque la palbra fin
