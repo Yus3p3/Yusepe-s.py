936748122ffe692064abea276d9d51e043ac2184
Yus3p3 <111707543+Yus3p3@users.noreply.github.com>
2022-08-21 19:31:46 -0600

    Create año-bisiesto.py

---
+++ año-bisiesto.py
@@ -0,0 +1,6 @@
+c=input("ingrese contraseña")
+while c != "ankaramessi":
+ print =(input(" error, vuelva a intetarlo"))
+ c=input("Ingrese contraseña")
+else:print("Genio entraste a la pc")
+#Este programa te dice dependiendo del año que ingrese el usuario si ese año fue o va aser bisiesto
